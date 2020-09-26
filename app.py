92a9fa8fc9e74a844528691706ded9cd7a0bb3f4
dhf145 <71777200+dhf145@users.noreply.github.com>
2020-09-26 17:02:36 +0700

    deploy to heroku

--- app.py
+++ app.py
@@ -21,6 +21,9 @@ def start():
                     # if you want to turn off the case sensitive like: priktiw, Prikitiw, pRiKiTiw
                     # just use lower(message) and check it, but please remove the replace function line
                     if "pasuju" in message:
+                        if ["wts", "wtb", "shopee", "tokopedia", "toped"] in message:
+                            print("DM will be deleted because contains bad keyword...")
+                            tw.delete_dm(id)
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
                                 print("DM will be posted")
@@ -38,10 +41,8 @@ def start():
                         print("DM will be deleted because does not contains keyword..")
                         tw.delete_dm(id)
 
-                    if ["wts","wtb","shopee","tokopedia","toped"] in message:
-                        print("DM will be deleted because contains bad keyword...")
-                        tw.delete_dm(id)
-                        
+
+
             dms = list()
 
         else:
